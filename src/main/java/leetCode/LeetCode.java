3776fabcf4d325dc0ecd4cfca829caf707fd70cb
zhangshihao <zhangshihao@hualala.com>
2020-11-04 16:11:17 +0800

    leeCode

--- src/main/java/leetCode/LeetCode.java
+++ src/main/java/leetCode/LeetCode.java
@@ -14,6 +14,89 @@ public class LeetCode {
 
 
     /**
+     *罗马数字包含以下七种字符： I， V， X， L，C，D 和 M。
+     *
+     * 字符          数值
+     * I             1
+     * V             5
+     * X             10
+     * L             50
+     * C             100
+     * D             500
+     * M             1000
+     * 例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+     *
+     * 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9
+     * 表示为 IX。这个特殊的规则只适用于以下六种情况：
+     *
+     * I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+     * X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+     * C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+     * 给定一个整数，将其转为罗马数字。输入确保在 1 到 3999 的范围内。
+     * @param num
+     * @return
+     */
+    public String intToRoman(int num) {
+        int[] numbers = {1,4,5,9,10,40,50,90,100,400,500,900,1000};
+        String[] romans = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+        //贪心算法
+        StringBuilder res = new StringBuilder();
+        for (int i = numbers.length-1; i >= 0 && num >= 0; i--) {
+            // Repeat while the current symbol still fits into num.
+            while (numbers[i] <= num) {
+                num -= numbers[i];
+                res.append(romans[i]);
+            }
+        }
+        return res.toString();
+    }
+
+
+
+    /**
+     * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)
+     * 。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/container-with-most-water
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param height
+     * @return
+     */
+    public int maxArea(int[] height) {
+        /**
+         * 解法一：暴力解法
+         */
+        int cap = 0;
+        //双指针
+        for (int i = 0; i < height.length; i++) {
+            for (int j = i+1; j < height.length; j++) {
+                int newCap = Math.min(height[i],height[j]) * (j - i);
+                cap = Math.max(cap,newCap);
+            }
+        }
+
+
+        /**
+         * 解法二：双指针
+         */
+        int head = 0,tail = height.length-1;
+        cap = 0;
+        while (head < tail){
+            int newCap = Math.min(height[head],height[tail])*(tail-head);
+            cap = Math.max(cap,newCap);
+            //哪个指针的值小移动哪个指针
+            int i = height[head] < height[tail] ? head++ : tail--;
+            //超简洁写法
+            /*cap = height[head] < height[tail] ?
+                    Math.max(cap,(tail-head)*height[head++]) :
+                    Math.max(cap,(tail-head)*height[tail--]);*/
+        }
+        return cap;
+    }
+
+
+    /**
      * 窗口法求
      * @param target
      * @return
